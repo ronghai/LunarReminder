d9df3eeb78fa8640fada132b89dd72fe0d9a4e1b
Ronghai Wei <ronghai.wei@me.com>
2012-05-26 11:17:45 -0400

    .......

---
+++ LunarReminder/LunarEventsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  LunarEventsTableViewController.h
+//  LunarReminder
+//
+//  Created by 韦 荣海 on 12-5-25.
+//  Copyright (c) 2012年 ronghai.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LunarEventsTableViewController : UITableViewController
+
+@property (nonatomic, strong)  NSMutableArray * lunarEvents; // of CalculatorBrain programs
+
+@end
